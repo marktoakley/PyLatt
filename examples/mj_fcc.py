dd33939faef49a6fef708bcede6970fdfdc2ee32
Mark Oakley <marktoakley@hotmail.com>
2015-09-22 23:44:23 +0100

    Added the Miyazawa-Hernigan potential (just the pairwise part for now).

--- examples/mj_fcc.py
+++ examples/mj_fcc.py
@@ -1,19 +1,19 @@
 '''
-A simple search on a cubic 3D lattice using the HP potential.
+A search on a face-centered cubic lattice using the MJ potential.
 
 @author: Mark Oakley
 '''
-from lattice.model import HP
+from lattice.model import MJ
 from lattice.moves import *
 from lattice.plotter import plot_3d
 from lattice.search import RandomSearch
 
-sequence = "PHPPHPHHHPHHPHHHHH"
-model = HP(sequence)
-lattice = CubicLattice()
+sequence = "LMVGGVVIA"
+model = MJ(sequence)
+lattice = FCCLattice()
 
 search = RandomSearch(lattice, model)
 
-structure = search.run(100)
+structure = search.run(1000)
 
 plot_3d(structure, model)
