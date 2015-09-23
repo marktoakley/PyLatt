6e173d2594b8b78fab68cf2f117ebad961cc632b
Mark Oakley <marktoakley@hotmail.com>
2015-09-23 18:57:27 +0100

    Added MonteCarlo search class.

--- examples/mj_fcc.py
+++ examples/mj_fcc.py
@@ -1,18 +1,18 @@
 '''
-A search on a face-centered cubic lattice using the MJ potential.
+A Monte Carlo search on a face-centered cubic lattice using the MJ potential.
 
 @author: Mark Oakley
 '''
 from pylatt.model import MJ
 from pylatt.lattice import *
 from pylatt.plotter import plot_3d
-from pylatt.search import RandomSearch
+from pylatt.search import MonteCarlo
 
 sequence = "LMVGGVVIA"
 model = MJ(sequence)
 lattice = FCCLattice()
 
-search = RandomSearch(lattice, model)
+search = MonteCarlo(lattice, model)
 
 structure = search.run(1000)
 
--- pylatt/search.py
+++ pylatt/search.py
@@ -2,6 +2,7 @@
 @author: Mark Oakley
 '''
 from pylatt.lattice_structure import LatticeStructureFactory
+from pylatt.reptate import Reptator
 
 class RandomSearch:
     '''Repeatedly generates random structures.'''
@@ -31,3 +32,24 @@ class RandomSearch:
                 best_structure = structure
             #print i, energy, best_energy
         return best_structure
+    
+class MonteCarlo:
+    '''Metropolis Monte Carlo (without the Metropolis test at the moment).'''
+    def __init__(self, lattice, model):
+        self.lattice = lattice
+        self.model = model
+        self.last_structure = LatticeStructureFactory(self.model.natoms, self.lattice).random_avoid()
+        self.model.calculate_energy(self.last_structure)
+        self.best_structure = self.last_structure
+        self.mover = Reptator()
+        
+    def run(self, steps):
+        for i in range(1, steps):
+            structure = self.mover.move(self.last_structure)
+            energy = self.model.calculate_energy(structure)
+            if structure.energy < self.best_structure.energy:
+                self.best_structure = structure
+            # Metropolis stuff goes here
+            #print i, energy, self.best_structure.energy
+            self.last_structure = structure
+        return self.best_structure
