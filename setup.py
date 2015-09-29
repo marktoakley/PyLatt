ccfff23c32db010125b85d324c053072e1799721
Mark Oakley <marktoakley@hotmail.com>
2015-09-29 18:39:32 +0100

    Version 0.1: pylatt now has simple Monte Carlo search implemented.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='PyLattice',
-    version='0',
+    version='0.1',
     author='Mark Oakley',
     author_email='marktoakley@hotmail.com',
     packages=['pylatt'],
