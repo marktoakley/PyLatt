7b74bb11fffe4110f2d4e18570080d2531643287
Mark Oakley <marktoakley@hotmail.com>
2015-10-19 19:55:46 +0100

    Update to v0.2.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='PyLattice',
-    version='0.1',
+    version='0.2',
     author='Mark Oakley',
     author_email='marktoakley@hotmail.com',
     packages=['pylatt'],
